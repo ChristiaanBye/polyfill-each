92039c45e8a70639dcb1fb79d29f0dacb2eccda9
Christiaan Bye <christiaanbye@gmail.com>
2021-08-04 22:30:09 +0200

    Added some remarks to describe the code and credit for a code sample used

--- polyfill-each.php
+++ polyfill-each.php
@@ -1,15 +1,24 @@
 <?php
 
 if (!function_exists('each')) {
+    /**
+     * @param array $array Although the actual function accepted objects, it was discouraged to pass them. Hence for the
+     *                     shim only an array is supported.
+     *
+     * @return array|false
+     */
     function each(array &$array)
     {
         $key   = key($array);
         $value = current($array);
 
         if ($key === null) {
+            // key() returns null if the array pointer is beyond the list of element or if the array is empty. If the
+            // same scenario occurred in the each() function, a false was returned instead. Hence returning false here
             return false;
         }
 
+        // Advance the array pointer before returning
         next($array);
 
         return array(
--- test.php
+++ test.php
@@ -8,6 +8,7 @@ $array = array(
     'key3' => 'value3',
 );
 
+// Credit for below code sample: https://wiki.php.net/rfc/deprecations_php_7_2#each
 while (list($key, $val) = each($array)) {
     echo "$key => $val\n";
 }
